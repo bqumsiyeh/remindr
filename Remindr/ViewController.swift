17352eb0b9d1129a77b46a6556974d0d74e9adfa
Basel Qumsiyeh <basel.qumsiyeh@veeva.com>
2016-08-01 00:14:49 -0700

    Initial Commit

---
+++ Remindr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Remindr
+//
+//  Created by Basel Qumsiyeh on 8/1/16.
+//  Copyright © 2016 Basel Qumsiyeh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
